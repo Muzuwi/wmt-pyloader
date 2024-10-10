403de2684c548c9959ec88d1b2280d92da5650c4
Muzuwi <kedziora99@gmail.com>
2024-10-10 21:54:34 +0200

    Reformat code with Black

--- src/wmt-pyloader.py
+++ src/wmt-pyloader.py
@@ -8,31 +8,32 @@ DEV_NODE = "/dev/wmtdetect"
 PROC_WMT_DBG = "/proc/driver/wmt_dbg"
 PROC_WMT_AEE = "/proc/driver/wmt_aee"
 CHIPID = "-1"
-CHIPID_VALIDITY_BYTES = b'\x20\x66\x00\x00\x28\x66\x00\x00\x30\x66\x00\x00\x32\x66\x00\x00\x72\x65\x00\x00\x82\x65\x00\x00\x92\x65\x00\x00\x27\x81\x00\x00\x71\x65\x00\x00\x52\x67\x00\x00\x35\x67\x00\x00\x21\x03\x00\x00\x35\x03\x00\x00\x37\x03\x00\x00\x63\x81\x00\x00\x80\x65\x00\x00\x55\x67\x00\x00\x26\x03\x00\x00\x97\x67\x00\x00\x79\x02\x00\x00\x57\x67\x00\x00\x51\x05\x00\x00\x67\x81\x00\x00\x59\x67\x00\x00\x07\x05\x00\x00\x63\x67\x00\x00\x90\x06\x00\x00\x70\x65\x00\x00\x13\x07\x00\x00\x75\x67\x00\x00\x88\x07\x00\x00\x71\x67\x00\x00\x65\x67\x00\x00\x67\x39\x00\x00\x61\x67\x00\x00\x79\x67\x00\x00\x68\x67\x00\x00\x85\x67\x00\x00\x73\x68\x00\x00\x68\x81\x00\x00\x53\x68\x00\x00\x33\x68\x00\x00\x81\x67\x00\x00'
-CHIPID_VALIDITY =  struct.unpack('<{}I'.format(len(CHIPID_VALIDITY_BYTES) // 4), CHIPID_VALIDITY_BYTES)
+CHIPID_VALIDITY_BYTES = b"\x20\x66\x00\x00\x28\x66\x00\x00\x30\x66\x00\x00\x32\x66\x00\x00\x72\x65\x00\x00\x82\x65\x00\x00\x92\x65\x00\x00\x27\x81\x00\x00\x71\x65\x00\x00\x52\x67\x00\x00\x35\x67\x00\x00\x21\x03\x00\x00\x35\x03\x00\x00\x37\x03\x00\x00\x63\x81\x00\x00\x80\x65\x00\x00\x55\x67\x00\x00\x26\x03\x00\x00\x97\x67\x00\x00\x79\x02\x00\x00\x57\x67\x00\x00\x51\x05\x00\x00\x67\x81\x00\x00\x59\x67\x00\x00\x07\x05\x00\x00\x63\x67\x00\x00\x90\x06\x00\x00\x70\x65\x00\x00\x13\x07\x00\x00\x75\x67\x00\x00\x88\x07\x00\x00\x71\x67\x00\x00\x65\x67\x00\x00\x67\x39\x00\x00\x61\x67\x00\x00\x79\x67\x00\x00\x68\x67\x00\x00\x85\x67\x00\x00\x73\x68\x00\x00\x68\x81\x00\x00\x53\x68\x00\x00\x33\x68\x00\x00\x81\x67\x00\x00"
+CHIPID_VALIDITY = struct.unpack(
+    "<{}I".format(len(CHIPID_VALIDITY_BYTES) // 4), CHIPID_VALIDITY_BYTES
+)
 
 IOCTL_BASE_R = 0x80047700
 IOCTL_BASE_W = 0x40047700
-COMBO_IOCTL_GET_CHIP_ID         = (IOCTL_BASE_R + 0  ) & 0xffffffff
-COMBO_IOCTL_SET_CHIP_ID         = (IOCTL_BASE_W + 1  ) & 0xffffffff
-COMBO_IOCTL_EXT_CHIP_DETECT     = (IOCTL_BASE_R + 2  ) & 0xffffffff
-COMBO_IOCTL_GET_SOC_CHIP_ID     = (IOCTL_BASE_R + 3  ) & 0xffffffff
-COMBO_IOCTL_DO_MODULE_INIT      = (IOCTL_BASE_R + 4  ) & 0xffffffff
-COMBO_IOCTL_MODULE_CLEANUP      = (IOCTL_BASE_R + 5  ) & 0xffffffff
-COMBO_IOCTL_EXT_CHIP_PWR_ON     = (IOCTL_BASE_R + 6  ) & 0xffffffff
-COMBO_IOCTL_EXT_CHIP_PWR_OFF    = (IOCTL_BASE_R + 7  ) & 0xffffffff
-COMBO_IOCTL_DO_SDIO_AUDOK       = (IOCTL_BASE_R + 8  ) & 0xffffffff
-COMBO_IOCTL_GET_ADIE_CHIP_ID    = (IOCTL_BASE_R + 9  ) & 0xffffffff
-COMBO_IOCTL_CONNSYS_SOC_HW_INIT = (IOCTL_BASE_R + 10 ) & 0xffffffff
-
+COMBO_IOCTL_GET_CHIP_ID = (IOCTL_BASE_R + 0) & 0xFFFFFFFF
+COMBO_IOCTL_SET_CHIP_ID = (IOCTL_BASE_W + 1) & 0xFFFFFFFF
+COMBO_IOCTL_EXT_CHIP_DETECT = (IOCTL_BASE_R + 2) & 0xFFFFFFFF
+COMBO_IOCTL_GET_SOC_CHIP_ID = (IOCTL_BASE_R + 3) & 0xFFFFFFFF
+COMBO_IOCTL_DO_MODULE_INIT = (IOCTL_BASE_R + 4) & 0xFFFFFFFF
+COMBO_IOCTL_MODULE_CLEANUP = (IOCTL_BASE_R + 5) & 0xFFFFFFFF
+COMBO_IOCTL_EXT_CHIP_PWR_ON = (IOCTL_BASE_R + 6) & 0xFFFFFFFF
+COMBO_IOCTL_EXT_CHIP_PWR_OFF = (IOCTL_BASE_R + 7) & 0xFFFFFFFF
+COMBO_IOCTL_DO_SDIO_AUDOK = (IOCTL_BASE_R + 8) & 0xFFFFFFFF
+COMBO_IOCTL_GET_ADIE_CHIP_ID = (IOCTL_BASE_R + 9) & 0xFFFFFFFF
+COMBO_IOCTL_CONNSYS_SOC_HW_INIT = (IOCTL_BASE_R + 10) & 0xFFFFFFFF
 
 
 def identify_chip_type_magic(err: int, chipid: int):
-    """ I have no idea what this does, it seems important
+    """I have no idea what this does, it seems important
 
     Takes in err from SET_CHIP_ID and chipid used as arg for the ioctl
     """
-    fallback = chipid & 0xffffffff
+    fallback = chipid & 0xFFFFFFFF
 
     if err < 0x507:
         if err < 0x321 + 0x17:
@@ -60,7 +61,7 @@ def identify_chip_type_magic(err: int, chipid: int):
         if err in [0x713]:
             return 0x6775
         if err in [0x788]:
-                return 0x6771
+            return 0x6771
         return fallback
 
 
@@ -108,13 +109,13 @@ def do_loader() -> int:
                 print(f"wmt_pyloader: External combo power-on failed with err({err})")
                 fd.close()
                 return 1
-            
+
             is_inited = True
             chip_id_ioctl = COMBO_IOCTL_GET_SOC_CHIP_ID
             print("wmt_pyloader: SOC chip no need do combo chip power on")
 
         # Read chip ID
-        chipid = do_ioctl(fd, chip_id_ioctl, 0) & 0xffffffff
+        chipid = do_ioctl(fd, chip_id_ioctl, 0) & 0xFFFFFFFF
         print(f"wmt_pyloader: Detected chip ID: {hex(chipid)}")
 
         if is_inited:
@@ -140,8 +141,8 @@ def do_loader() -> int:
             return 1
         print("wmt_pyloader: External combo chip power off done")
 
-    err = do_ioctl(fd, COMBO_IOCTL_SET_CHIP_ID, chipid & 0xffffffff)
-    chip_type = identify_chip_type_magic(err, chipid & 0xffffffff)
+    err = do_ioctl(fd, COMBO_IOCTL_SET_CHIP_ID, chipid & 0xFFFFFFFF)
+    chip_type = identify_chip_type_magic(err, chipid & 0xFFFFFFFF)
 
     if chip_type is None:
         # Doesn't look fatal?
@@ -155,7 +156,9 @@ def do_loader() -> int:
             else:
                 print(f"wmt_pyloader: COMBO_IOCTL_DO_MODULE_INIT failed: err{hex(err)}")
         else:
-            print(f"wmt_pyloader: COMBO_IOCTL_MODULE_CLEANUP call failed: err({hex(err)})")
+            print(
+                f"wmt_pyloader: COMBO_IOCTL_MODULE_CLEANUP call failed: err({hex(err)})"
+            )
 
     adie_chipid = do_ioctl(fd, COMBO_IOCTL_GET_ADIE_CHIP_ID, 0)
     if adie_chipid == -1:
@@ -177,16 +180,24 @@ def do_loader() -> int:
         print(f"wmt_pyloader: chown({PROC_WMT_AEE}, 2000, 1000) failed: {e}")
         return 1
 
+    return 0
+
 
 def do_launcher() -> int:
     return 0
 
 
 def main() -> int:
-    parser = argparse.ArgumentParser(prog="wmt_pyloader",
-                                     description="Mediatek WiFi Loader")
-    parser.add_argument("--skip-loader", action="store_true", default=False,
-                        dest='skip_loader', help='Do not perform Loader step')
+    parser = argparse.ArgumentParser(
+        prog="wmt_pyloader", description="Mediatek WiFi Loader"
+    )
+    parser.add_argument(
+        "--skip-loader",
+        action="store_true",
+        default=False,
+        dest="skip_loader",
+        help="Do not perform Loader step",
+    )
     args = parser.parse_args()
 
     if not args.skip_loader:
@@ -206,5 +217,3 @@ def main() -> int:
 
 if __name__ == "__main__":
     exit(main())
-
-
